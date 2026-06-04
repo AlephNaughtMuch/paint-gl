1d6339438fdbe5c734a9e6dc5fad2737eda609f7
AlephNaughtMuch <shubhamkapoor9778@gmail.com>
2026-06-03 22:35:52 -0700

    Implementing camera, WASD and mouse input processing for moving the camera for interactive model viewing.

---
+++ src/camera.cpp
@@ -0,0 +1,55 @@
+#include "camera.h"
+
+#include <algorithm>
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Constructor
+Camera::Camera(glm::vec3 pos, glm::vec3 upVector, glm::vec3 fwd) {
+    // Set camera properties
+    position = pos;
+    forward = fwd;
+    up = upVector;
+
+    updateVectors();
+}
+
+void Camera::updateVectors() {
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    forward = glm::normalize(front);
+    right = glm::normalize(glm::cross(forward, up));
+}
+
+glm::mat4 Camera::getViewMatrix() {
+    return glm::lookAt(position, position + forward, up);
+}
+
+glm::mat4 Camera::getProjectionMatrix(float aspectRatio) {
+    return glm::perspective(glm::radians(fov), aspectRatio, zNear, zFar);
+}
+
+void Camera::processKeyboard(CameraMovement direction, float deltaTime) {
+    if (direction == CameraMovement::FORWARD) {
+        position += forward * speed * deltaTime;
+    } else if (direction == CameraMovement::BACKWARD) {
+        position -= forward * speed * deltaTime;
+    } else if (direction == CameraMovement:: LEFT) {
+        position -= right * speed * deltaTime;
+    } else if (direction == CameraMovement::RIGHT) {
+        position += right * speed * deltaTime;
+    }
+}
+
+void Camera::processMouse(float xOffset, float yOffset) {
+    yaw += xOffset * sensitivity;
+    pitch += yOffset * sensitivity;
+
+    pitch = std::min(pitch, 89.0f);
+    pitch = std::max(pitch, -89.0f);
+    // clamp pitch
+    updateVectors();
+}
